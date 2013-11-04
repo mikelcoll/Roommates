b135ad35d656af3c45c0c3b0fd96b8305341d2f0
gemma-ferreras <gemma.fbar@gmail.com>
2013-11-04 19:25:01 +0100

    DB

---
+++ Roommates/DBManager.h
@@ -0,0 +1,23 @@
+//
+//  DBManager.h
+//  Roommates
+//
+//  Created by Gemma Ferreras on 04/11/13.
+//  Copyright (c) 2013 rich and famous. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+@interface DBManager : NSObject
+{
+    NSString *databasePath;
+}
+
++(DBManager*)getSharedInstance;
+-(BOOL)createDB;
+-(BOOL) saveData:(NSString*)registerNumber name:(NSString*)name
+      department:(NSString*)department year:(NSString*)year;
+-(NSArray*) findByRegisterNumber:(NSString*)registerNumber;
+
+@end
